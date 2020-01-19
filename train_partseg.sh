a757d1bd86e9e92379aad4e3e381d69e5e266d4c
Nicolas Chaulet <nicolas.chaulet@gmail.com>
2020-01-19 12:33:52 +0000

    It trains

--- train_partseg.sh
+++ train_partseg.sh
@@ -5,4 +5,4 @@ log_name="PartSeg_LOG_"$now""
 export CUDA_VISIBLE_DEVICES=0
 python -u train_partseg.py \
 --config cfgs/config_msn_partseg.yaml \
-2>&1|tee log/$log_name.log &
+2>&1|tee log/$log_name.log 
